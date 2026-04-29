43585977b9bcfa43d01d59335d13d900a671bcc9
Bangulli <lorenz.achim.kuhn@gmail.com>
2026-04-29 12:02:20 +0000

    Add eval scripts

--- eval/clustering.py
+++ eval/clustering.py
@@ -1,16 +1,17 @@
 ## evaluate method for metadata-embedding similarity
+import os, torch, pathlib as pl, numpy as np, sys, json
+sys.path.append(os.path.join(os.path.dirname(__file__), "."))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 from BPTorch.datasets import BigPictureRepository, WsiDicomDataset
 from torch.utils.data import DataLoader
 from BPTorch.utils import bptorch_collate
 from torch.nn.functional import avg_pool2d
 from pprint import pprint
-from src.model.arch import H0_mini_for_AutoEncoding
-from src.losses.loss_fusion import SIPE_Loss
+from src.model.arch import H0_mini_for_Adversarial
 from torchvision.transforms import ToPILImage
 from src.utils.transfroms import UnNormalize
 from src.trainer.trainer import Trainer
-import os, torch, pathlib as pl, numpy as np
 from tqdm import tqdm
 from BPTorch.utils import bptorch_collate
 from sklearn.preprocessing import LabelEncoder
@@ -19,20 +20,53 @@ from umap import UMAP
 from sklearn.metrics import davies_bouldin_score
 from src.utils.misc import make_name_from_list
 from src.utils.visu import plot_dim_red_clust, make_or_load_cmap
+from typing import Literal
 # pip install "BPTorch @ git+https://github.com/Bangulli/BPTorch"
 
-if __name__ == '__main__':
-    datapath = pl.Path('/mnt/nas6/data/BigPicture_CBIR/datasets/BPTorch/fold_0/BPR.json')
-    inf_name = 'infered'
-    recomp = input('Recompute embeddings? (y/n) : ').lower() == 'y'
+class InferenceWrapper():
+    def __init__(self, model, mode: Literal['cls', 'patch_token', 'ours'], device='cuda:0'):
+        self.model = model
+        self.model.eval()
+        self.model.device = device
+        self.model.to(device)
+        self.device = device
+        if mode.lower() in ['cls', 'patch_token', 'ours']: self.mode = mode.lower()
+        else: raise RuntimeError(f'Unkown mode {mode}')
+        
+    def __call__(self, batch):
+        if self.mode == 'cls':
+            x = batch['image'].to(self.device)
+            if len(x.shape)==3: x = x.unsqueeze(0) ## add batch dim if neccessary
+            tok = self.model.backbone(x)[:, 0, :]
+            return tok.detach().cpu().squeeze()
+        elif self.mode == 'patch_token':
+            x = batch['image'].to(self.device)
+            if len(x.shape)==3: x = x.unsqueeze(0) ## add batch dim if neccessary
+            tok = self.model.backbone(x)[:, 5:, :].permute(0, 2, 1)
+            tok = tok.reshape(tok.shape[0], 768, 16, 16)
+            return avg_pool2d(tok, kernel_size=16).detach().cpu().squeeze()
+        elif self.mode == 'ours':
+            tok = self.model(batch)
+            s = avg_pool2d(tok[:, :self.model.emb_stain_size], kernel_size=16).detach().cpu().squeeze()
+            z = avg_pool2d(tok[:, self.model.emb_stain_size:], kernel_size=16).detach().cpu().squeeze()
+            sz = torch.cat([s, z], dim=1)
+            return s, z, sz
 
-    ####### data i/o
-    if (not os.path.exists(datapath.parent/inf_name)) or recomp:
+def run_inference(datapath, outputpath, inf_name, methods, trainer_path):
+    with open('/home/lorenz/BigPicture/SIPE/classes.json', 'r') as f:
+        classes = json.load(f)
+    os.makedirs(outputpath, exist_ok=True)
+    for eval_method in methods:
         ###### model i/o
-        trainer = Trainer(H0_mini_for_AutoEncoding(device='cuda:1'), None, wdir='/home/lorenz/BigPicture/SIPE/SIPE-1M')
-        model = trainer.load_best_model()
-        model.eval()
-        model.to(model.device)
+        if eval_method in ['cls', 'patch_token']: ## make default config model with default weights
+            model = H0_mini_for_Adversarial(classes, device='cuda:1')
+        else: 
+            trainer = Trainer(H0_mini_for_Adversarial(classes, device='cuda:1'), None, wdir=trainer_path)
+            model = trainer.load_best_model()
+
+        inferer = InferenceWrapper(model, eval_method)
+            
+        ###### data i/o
         kwargs = WsiDicomDataset.get_default_kwargs()
         kwargs['transforms'] = model.transform
             
@@ -43,67 +77,116 @@ if __name__ == '__main__':
     
         dl = torch.utils.data.DataLoader(ds, collate_fn=bptorch_collate, batch_size=128)
         all_embeddings = []
+        all_s = []
+        all_z = []
+        all_sz = []
         all_labels_stain = []
         all_labels_site = []
         for batch in tqdm(dl, desc='Infering Batches'):
             torch.cuda.empty_cache()
-            _, emb = model(batch)
+            
+            if eval_method  in ['cls', 'patch_token']:
+                emb = inferer(batch)
+                all_embeddings.append(emb)
+            else:
+                all_embeddings = False
+                s, z, sz = inferer(batch)
+                all_s.append(s)
+                all_z.append(z)
+                all_sz.append(sz)
+                
             lbls = [make_name_from_list(samp['staining']) for samp in batch['metadata']]
             lbls_site = [make_name_from_list(samp['organ']) for samp in batch['metadata']]
             
-            all_embeddings.append(avg_pool2d(emb.detach().cpu(), kernel_size=16))
             all_labels_stain += lbls
             all_labels_site += lbls_site
             
+        if all_embeddings:
+            all_embeddings = torch.concat(all_embeddings, dim=0)
+            all_embeddings = all_embeddings.numpy()
+            print(all_embeddings.shape)
+            os.makedirs(outputpath/inf_name, exist_ok=True)
+            np.save(outputpath/inf_name/f'{eval_method}_embeddings.npy', all_embeddings)
+        else:
+            all_s = torch.concat(all_s, dim=0)
+            all_s = all_s.numpy()
+            print(all_s.shape)
+            os.makedirs(outputpath/inf_name, exist_ok=True)
+            np.save(outputpath/inf_name/f'{eval_method}_s.npy', all_s)
             
-        all_embeddings = torch.concat(all_embeddings, dim=0)
-        all_embeddings = all_embeddings.numpy()
-        print(all_embeddings.shape)
-        os.makedirs(datapath.parent/inf_name, exist_ok=True)
-        np.save(datapath.parent/inf_name/'embeddings.npy', all_embeddings)
+            all_z = torch.concat(all_z, dim=0)
+            all_z = all_z.numpy()
+            print(all_z.shape)
+            os.makedirs(outputpath/inf_name, exist_ok=True)
+            np.save(outputpath/inf_name/f'{eval_method}_z.npy', all_z)
+        
+        
+            all_sz = torch.concat(all_sz, dim=0)
+            all_sz = all_sz.numpy()
+            print(all_sz.shape)
+            os.makedirs(outputpath/inf_name, exist_ok=True)
+            np.save(outputpath/inf_name/f'{eval_method}_sz.npy', all_sz)
+        
         
         enc_stain = LabelEncoder()
         all_labels_stain = enc_stain.fit_transform(all_labels_stain)
         enc_stain = enc_stain.classes_
-        np.save(datapath.parent/inf_name/'stainings.npy', all_labels_stain)
-        np.save(datapath.parent/inf_name/'map_stain.npy', enc_stain)
+        np.save(outputpath/inf_name/'stainings.npy', all_labels_stain)
+        np.save(outputpath/inf_name/'map_stain.npy', enc_stain)
         
         enc_site = LabelEncoder()
         all_labels_site = enc_site.fit_transform(all_labels_site)
         enc_site = enc_site.classes_
-        np.save(datapath.parent/inf_name/'organs.npy', all_labels_site)
-        np.save(datapath.parent/inf_name/'map_site.npy', enc_site)
+        np.save(outputpath/inf_name/'organs.npy', all_labels_site)
+        np.save(outputpath/inf_name/'map_site.npy', enc_site)
         
-    else:
-        all_embeddings = np.load(datapath.parent/inf_name/'embeddings.npy')
-        
-        all_labels_stain = np.load(datapath.parent/inf_name/'stainings.npy')
-        enc_stain = np.load(datapath.parent/inf_name/'map_stain.npy')
-        
-        all_labels_site = np.load(datapath.parent/inf_name/'organs.npy')
-        enc_site = np.load(datapath.parent/inf_name/'map_site.npy')
-        
+def eval_clust(emb_path, stain_path, stain_map, site_path, site_map, outpath):
+    os.makedirs(outpath, exist_ok=True)
+    name = emb_path.name.split('.')[0]
+    all_embeddings = np.load(emb_path)
+    
+    all_labels_stain = np.load(stain_path)
+    enc_stain = np.load(stain_map)
+    
+    all_labels_site = np.load(site_path)
+    enc_site = np.load(site_map)
+    
     ############################################################################### make plots
     tsne=TSNE(n_components=2, random_state=42, perplexity=30, init="pca", learning_rate="auto", metric="cosine", early_exaggeration=12)
     projections_tsne = tsne.fit_transform(all_embeddings)
     print(f"Davies-Bouldin for clustering staining in the whole vector: {davies_bouldin_score(all_embeddings, all_labels_stain)}")
     print(f"Davies-Bouldin for clustering organ in the whole vector: {davies_bouldin_score(all_embeddings, all_labels_site)}")
     
-    plot_dim_red_clust(projections_tsne, all_labels_stain, make_or_load_cmap(all_labels_stain, datapath.parent/inf_name/'stain_cmap.json'), datapath.parent/inf_name/'tsne-stain-ALL.png', 't-SNE for Stain')
-    plot_dim_red_clust(projections_tsne, all_labels_site, make_or_load_cmap(all_labels_site, datapath.parent/inf_name/'site_cmap.json'), datapath.parent/inf_name/'tsne-site-ALL.png', 't-SNE for Site')
+    plot_dim_red_clust(projections_tsne, all_labels_stain, make_or_load_cmap(all_labels_stain, outpath/'stain_cmap.json'), outpath/f'{name}-tsne-stain-ALL.png', f'{name} t-SNE for Stain')
+    plot_dim_red_clust(projections_tsne, all_labels_site, make_or_load_cmap(all_labels_site, outpath/'site_cmap.json'), outpath/f'{name}-tsne-site-ALL.png', f'{name} t-SNE for Site')
     
-    tsne=TSNE(n_components=2, random_state=42, perplexity=30, init="pca", learning_rate="auto", metric="cosine", early_exaggeration=12)
-    projections_tsne = tsne.fit_transform(all_embeddings[:,:64])
-    print(f"Davies-Bouldin for clustering staining in the speciefied part: {davies_bouldin_score(all_embeddings[:,:64], all_labels_stain)}")
-    print(f"Davies-Bouldin for clustering organ in the specified part: {davies_bouldin_score(all_embeddings[:,:64], all_labels_site)}")
+    # tsne=TSNE(n_components=2, random_state=42, perplexity=30, init="pca", learning_rate="auto", metric="cosine", early_exaggeration=12)
+    # projections_tsne = tsne.fit_transform(all_embeddings[:,:64])
+    # print(f"Davies-Bouldin for clustering staining in the speciefied part: {davies_bouldin_score(all_embeddings[:,:64], all_labels_stain)}")
+    # print(f"Davies-Bouldin for clustering organ in the specified part: {davies_bouldin_score(all_embeddings[:,:64], all_labels_site)}")
     
-    plot_dim_red_clust(projections_tsne, all_labels_stain, make_or_load_cmap(all_labels_stain, datapath.parent/inf_name/'stain_cmap.json'), datapath.parent/inf_name/'tsne-stain-S.png', 't-SNE for Stain')
-    plot_dim_red_clust(projections_tsne, all_labels_site, make_or_load_cmap(all_labels_site, datapath.parent/inf_name/'site_cmap.json'), datapath.parent/inf_name/'tsne-site-S.png', 't-SNE for Site')
+    # plot_dim_red_clust(projections_tsne, all_labels_stain, make_or_load_cmap(all_labels_stain, outputpath/inf_name/'stain_cmap.json'), outputpath/inf_name/f'{name}-tsne-stain-S.png', f'{name} t-SNE for Stain')
+    # plot_dim_red_clust(projections_tsne, all_labels_site, make_or_load_cmap(all_labels_site, outputpath/inf_name/'site_cmap.json'), outputpath/inf_name/f'{name}-tsne-site-S.png', f'{name} t-SNE for Site')
     
-    tsne=TSNE(n_components=2, random_state=42, perplexity=30, init="pca", learning_rate="auto", metric="cosine", early_exaggeration=12)
-    projections_tsne = tsne.fit_transform(all_embeddings[:,64:])
-    print(f"Davies-Bouldin for clustering staining in the unspecified part: {davies_bouldin_score(all_embeddings[:,64:], all_labels_stain)}")
-    print(f"Davies-Bouldin for clustering organ in the unspecified part: {davies_bouldin_score(all_embeddings[:,64:], all_labels_site)}")
+    # tsne=TSNE(n_components=2, random_state=42, perplexity=30, init="pca", learning_rate="auto", metric="cosine", early_exaggeration=12)
+    # projections_tsne = tsne.fit_transform(all_embeddings[:,64:])
+    # print(f"Davies-Bouldin for clustering staining in the unspecified part: {davies_bouldin_score(all_embeddings[:,64:], all_labels_stain)}")
+    # print(f"Davies-Bouldin for clustering organ in the unspecified part: {davies_bouldin_score(all_embeddings[:,64:], all_labels_site)}")
     
-    plot_dim_red_clust(projections_tsne, all_labels_stain, make_or_load_cmap(all_labels_stain, datapath.parent/inf_name/'stain_cmap.json'), datapath.parent/inf_name/'tsne-stain-Z.png', 't-SNE for Stain')
-    plot_dim_red_clust(projections_tsne, all_labels_site, make_or_load_cmap(all_labels_site, datapath.parent/inf_name/'site_cmap.json'), datapath.parent/inf_name/'tsne-site-Z.png', 't-SNE for Site')
+    # plot_dim_red_clust(projections_tsne, all_labels_stain, make_or_load_cmap(all_labels_stain, outputpath/inf_name/'stain_cmap.json'), outputpath/inf_name/f'{name}-tsne-stain-Z.png', f'{name} t-SNE for Stain')
+    # plot_dim_red_clust(projections_tsne, all_labels_site, make_or_load_cmap(all_labels_site, outputpath/inf_name/'site_cmap.json'), outputpath/inf_name/f'{name}-tsne-site-Z.png', f'{name} t-SNE for Site')
+
+if __name__ == '__main__':
+    datapath = pl.Path('/mnt/nas6/data/BigPicture_CBIR/datasets/BPTorch/fold_0/BPR.json')
+    outputpath = pl.Path('v1_models/SIPE-1M-Curriculum/.results')
+    inf_name = 'infered'
+
+    run_inference(datapath, outputpath, inf_name, ['cls', 'patch_token', 'ours'], '/home/lorenz/BigPicture/SIPE/v1_models/SIPE-1M-Curriculum')
+        
+    for eval_res in ['cls_embeddings.npy', 'patch_token_embeddings.npy']:
+        print(f'---------------------------------- Evaluating {eval_res} Baseline ----------------------------------')
+        eval_clust(outputpath/inf_name/eval_res, outputpath/inf_name/'stainings.npy', outputpath/inf_name/'map_stain.npy', outputpath/inf_name/'organs.npy', outputpath/inf_name/'map_site.npy', outpath=outputpath/'baseline')
+        
+    for eval_res in ['ours_s.npy', 'ours_z.npy', 'ours_sz.npy']:  
+        print(f'---------------------------------- Evaluating {eval_res} Ours ----------------------------------')
+        eval_clust(outputpath/inf_name/eval_res, outputpath/inf_name/'stainings.npy', outputpath/inf_name/'map_stain.npy', outputpath/inf_name/'organs.npy', outputpath/inf_name/'map_site.npy', outpath=outputpath/'ours')
--- eval/virt_stain.py
+++ eval/virt_stain.py
@@ -1,54 +1,169 @@
 ## evaluate method for virtual staining
-
+import os, torch, pathlib as pl, numpy as np, sys, json
+sys.path.append(os.path.join(os.path.dirname(__file__), "."))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from pathlib import Path
 import os, torch
 import PIL
 from tqdm import tqdm
+from torchvision.transforms import ToTensor, Resize, Compose, Normalize, ToPILImage
+from src.utils.transfroms import UnNormalize
+from skimage.metrics import structural_similarity as ssim
+from pytorch_msssim import ms_ssim
+import torch
+from pprint import pprint
+from src.model.arch import H0_mini_for_Adversarial
+from src.trainer.trainer import Trainer
+from huggingface_hub import login
+
+def swap_stain(model, img_a, img_b):
+    to_resized_tensor = Compose([ToTensor(), Resize(5*224), Normalize([0.707223,0.578729,0.703617], [0.211883,0.230117,0.177517])])
+    to_regsized_pil = Compose([UnNormalize([0.707223,0.578729,0.703617], [0.211883,0.230117,0.177517]), Resize(1024), ToPILImage()])
+    
+    ten_aa = to_resized_tensor(img_a)
+    ten_bb = to_resized_tensor(img_b)
+    
+    out_ab = torch.zeros_like(ten_aa)
+    out_ba = torch.zeros_like(ten_bb)
+    
+    for w in range(0, 224*5, 224):
+        for h in range(0, 224*5, 224):
+            patch_aa = ten_aa[:, h:h+224, w:w+224]
+            patch_bb = ten_bb[:, h:h+224, w:w+224]
+            
+            batch = {'image': torch.stack([patch_aa, patch_bb])}
+            
+            embs = model(batch)
+            
+            s = embs[:, :model.emb_stain_size]
+            z = embs[:, model.emb_stain_size:]
+            
+            out_ab[:, h:h+224, w:w+224] = model.recon_image(torch.cat([s[0], z[1]], dim=0).unsqueeze(0))
+            out_ba[:, h:h+224, w:w+224] = model.recon_image(torch.cat([s[1], z[0]], dim=0).unsqueeze(0))
+    
+    return to_regsized_pil(out_ab), to_regsized_pil(out_ba)
+
+def get_metrics(img1, img2):
+    img2 = img2.convert(img1.mode)
+
+    arr1 = np.array(img1, dtype=np.float64)
+    arr2 = np.array(img2, dtype=np.float64)
+
+    # --- PSNR ---
+    mse = np.mean((arr1 - arr2) ** 2)
+    if mse == 0:
+        psnr = float("inf")
+    else:
+        max_val = 255.0
+        psnr = 10 * np.log10((max_val ** 2) / mse)
+
+    # --- PCC (Pearson Correlation Coefficient) ---
+    flat1, flat2 = arr1.flatten(), arr2.flatten()
+    pcc = np.corrcoef(flat1, flat2)[0, 1]
+
+    # --- SSIM ---
+    is_color = arr1.ndim == 3 and arr1.shape[2] == 3
+    ssim_val = ssim(
+        arr1.astype(np.uint8),
+        arr2.astype(np.uint8),
+        channel_axis=2 if is_color else None,
+        data_range=255,
+    )
+
+    # --- MS-SSIM (requires torch) ---
+    def to_tensor(arr):
+        t = torch.from_numpy(arr.astype(np.float32))
+        if t.ndim == 2:             # grayscale → (1, 1, H, W)
+            t = t.unsqueeze(0).unsqueeze(0)
+        else:                       # RGB → (1, C, H, W)
+            t = t.permute(2, 0, 1).unsqueeze(0)
+        return t
+
+    t1, t2 = to_tensor(arr1), to_tensor(arr2)
+    ms_ssim_val = ms_ssim(t1, t2, data_range=255.0, size_average=True).item()
+
+    return {
+        "PSNR":    float(psnr),
+        "PCC":     float(pcc),
+        "SSIM":    float(ssim_val),
+        "MS_SSIM": ms_ssim_val,
+    }
+    
+def mean(lst):
+    return sum(lst)/len(lst)
 
 def infer_and_eval_dir(model, output, dir='HER2'):
+    dir = f"{dir}/TrainValAB"
     output = Path(output)
     MIST = Path('/mnt/nas6/data/MIST')
 
     HandE = 'valA'
     other = 'valB'
-    
-    s_hande = torch.tensor() ## the stain vector signifying H&E
-    s_her2 = torch.tensor() ## the stain vector signifying H&HER2
 
     images = os.listdir(MIST/dir/HandE)
     
-    os.makedirs(output/dir/"he2other", exist_ok=True)
-    os.makedirs(output/dir/"other2he", exist_ok=True)
+    os.makedirs(output/dir.removesuffix('/TrainValAB')/"he2other", exist_ok=True)
+    os.makedirs(output/dir.removesuffix('/TrainValAB')/"other2he", exist_ok=True)
+    
+    results_other2he = {
+        'MS_SSIM': [],
+        'PCC': [],
+        'PSNR': [],
+        'SSIM': []
+    }
+    results_he2other = {
+        'MS_SSIM': [],
+        'PCC': [],
+        'PSNR': [],
+        'SSIM': []
+    }
     
     for img_id in tqdm(images, desc='Inference'):
         img_he = PIL.Image.open(MIST/dir/HandE/img_id)
         img_other = PIL.Image.open(MIST/dir/other/img_id)
         
-        ################# inference step ##################
-        ## HE 2 other
-        for patch in img_he:
-            pass ## infer patches
         ## fuse patches
-        img_he2other = PIL.Image() ##dummy
-        img_he2other.save(output/dir/"he2other"/img_id)
-        
-        ## other 2 HE
-        for patch in img_other:
-            pass ## infer patches
-        ## fuse patches
-        img_other2he = PIL.Image() ##dummy
-        img_other2he.save(output/dir/"other2he"/img_id)
+        img_other2he, img_he2other = swap_stain(model, img_he, img_other)
+        img_he2other.save(output/dir.removesuffix('/TrainValAB')/"he2other"/img_id)
+        img_other2he.save(output/dir.removesuffix('/TrainValAB')/"other2he"/img_id)
 
         
         ################# evaluation step ##################
         ## metrics from https://www.sciencedirect.com/science/article/pii/S2950261625000226
+        eval_other2he = get_metrics(img_he, img_other2he)
+        eval_he2other = get_metrics(img_other, img_he2other)
         
-        ## image based
-        PSNR = 0
-        PCC = 0
-        SSIM = 0
-        MS_SSIM = 0
+        for k in results_other2he.keys():
+            results_other2he[k].append(eval_other2he[k])
+            results_he2other[k].append(eval_he2other[k])
+    
+    for k in results_other2he.keys():
+        results_other2he[k]=mean(results_other2he[k])
+        results_he2other[k]=mean(results_he2other[k])
         
-        ## deep feature based
-        FID = 0
-        DISTS = 0
+    return results_he2other, results_other2he
+
+def test_virtual_staining(model_dir):
+    model_dir = Path(model_dir)
+    with open('/home/lorenz/BigPicture/SIPE/classes.json', 'r') as f:
+        classes = json.load(f)
+    trainer = Trainer(H0_mini_for_Adversarial(classes, device='cuda:0'), None, wdir=model_dir, device='cuda:0')
+    model = trainer.load_best_model()
+    
+    for stain in ['HER2', 'ER', 'Ki67', 'PR']:
+        results_he2other, results_other2he = infer_and_eval_dir(model, model_dir/'virtual-staining', stain)
+        
+        print('######################## RESULTS HE2OTHER ########################')
+        pprint(results_he2other)
+        
+        print('######################## RESULTS OTHER2HE ########################')
+        pprint(results_other2he)
+        
+        with open(model_dir/'virtual-staining'/stain/'results_he2other.json', 'w') as f:
+            json.dump(results_he2other, f, indent=4)
+            
+        with open(model_dir/'virtual-staining'/stain/'results_other2he.json', 'w') as f:
+            json.dump(results_other2he, f, indent=4)
+    
+if __name__ == '__main__':
+    test_virtual_staining('/home/lorenz/BigPicture/SIPE/v1_models/SIPE-1M-Curriculum')
